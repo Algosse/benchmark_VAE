49a9635e3b1b0a16db9629d81c9dd71c3e9dff62
= <=>
2024-03-09 16:48:55 +0100

    Fix for distributed training

--- src/pythae/trainers/vdvae_trainer/vdvae_trainer.py
+++ src/pythae/trainers/vdvae_trainer/vdvae_trainer.py
@@ -40,7 +40,10 @@ class VDVAETrainer(BaseTrainer):
         ]
         
         # Here, we can not simply draw z and use it for the decoder as we have multiple latent spaces.
-        generation = model.sample(min(inputs['data'].shape[0], 10)).recon_x.cpu().detach()
+        if self.distributed:
+            generation = model.module.sample(min(inputs['data'].shape[0], 10)).recon_x.cpu().detach()
+        else:
+            generation = model.sample(min(inputs['data'].shape[0], 10)).recon_x.cpu().detach()
         
         return (
             inputs['data'][: min(inputs['data'].shape[0], 10)],
